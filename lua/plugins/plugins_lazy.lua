f3a7b698716decf567ac37cdf14f0e1a282c08cc
toto-yousyo <hisakazu49@gmail.com>
2024-09-25 17:05:40 +0900

    imac

--- lua/plugins/plugins_lazy.lua
+++ lua/plugins/plugins_lazy.lua
@@ -374,6 +374,21 @@ return {
         },
       })
     end,
-  } 
+  }, 
+  {
+      "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        config = function()
+          require("chatgpt").setup({
+          api_key_cmd= os.getenv("OPENAI_API_KEY"), 
+      })
+        end,
+        dependencies = {
+          "MunifTanjim/nui.nvim",
+          "nvim-lua/plenary.nvim",
+          "folke/trouble.nvim",
+          "nvim-telescope/telescope.nvim"
+        }
+    }
 
 }
