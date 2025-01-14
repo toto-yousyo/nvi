4b0756b1e7052b4ac06fb8d08ece4b7541489504
Toto <hisakazu49@gmail.com>
2025-01-14 09:25:22 +0900

    macbook

--- lua/plugins/plugins_lazy.lua
+++ lua/plugins/plugins_lazy.lua
@@ -101,9 +101,20 @@ return {
 
       null_ls.setup({
           sources = {
+              null_ls.builtins.formatting.prettier, 
               null_ls.builtins.formatting.stylua,
               null_ls.builtins.diagnostics.eslint,
           },
+          on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = bufnr,
+                callback = function()
+                  vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+              })
+            end
+          end,
       })
     end, 
 }, 
