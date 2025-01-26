7710db6705d05b601b86337db77800354c9cea86
Toto <hisakazu49@gmail.com>
2025-01-27 08:39:54 +0900

    macbook

--- lua/plugins/plugins_lazy.lua
+++ lua/plugins/plugins_lazy.lua
@@ -11,9 +11,13 @@ return {
           sidebar = "transparent",
           floats = "transparent",
       }, 
+      on_highlights = function(hl, c)
+        hl.DiagnosticUnnecessary = { fg = "#777ea0" }
+      end, 
       on_colors = function(colors)
         colors.comment = "#777ea0"
         colors.bg_dark = "#161926"
+        colors.error = "#777ea0"
       end, 
     })
   end, 
