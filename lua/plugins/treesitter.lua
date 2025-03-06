468c9d87a2e6cbde5bf094b1c7f1bec5b6c8c37d
toto-yousyo <hisakazu49@gmail.com>
2025-03-06 18:18:00 +0900

    macbook

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,41 +1,24 @@
 return {
   {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufRead", "BufNewFile", "InsertEnter" },
-  build = ":TSUpdate",
-  config = function()
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufRead", "BufNewFile", "InsertEnter" },
+    build = ":TSUpdate",
+    config = function()
       local configs = require("nvim-treesitter.configs")
       configs.setup({
-          ensure_installed = {
-              "bash",
-              "comment",
-              "css",
-              "csv",
-              "diff",
-              "html",
-              "javascript",
-              "typescript", 
-              "tsx", 
-              "json",
-              "lua",
-              "markdown",
-              "python",
-              "sql",
-              "toml",
-              "vim",
-              "xml",
-              "yaml",
-              "regex",
-          },
+        ensure_installed = "all",
+        ignore_install = {
+          "org",
+        },
         contenxt_commentstring = {
-          enabel = true, 
-          enabel_autocmd = false, 
-        }, 
+          enabel = true,
+          enabel_autocmd = false,
+        },
         sync_install = false,
         auto_install = true,
---         highlight = { enable = true },
+        --         highlight = { enable = true },
         indent = { enable = true },
       })
-  end,
-},
+    end,
+  },
 }
