ac7789f049d5fd7f0bfac5df34fc86df3441f164
Toto <hisakazu49@gmail.com>
2025-08-01 08:40:57 +0900

    macbook

--- init.lua
+++ init.lua
@@ -1,29 +1,29 @@
 -- lazy.nvim boot strap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- local plugins
 local plugins = {
-    { import = "plugins" },
+  { import = "plugins" },
 }
 
 -- local opts
 local opts = {
-    root = vim.fn.stdpath("data") .. "/lazy",
-    lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
-    concurrency = 10,
-    checker = { enabled = true },
-    log = { level = "info" },
+  root = vim.fn.stdpath("data") .. "/lazy",
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
+  concurrency = 10,
+  checker = { enabled = true },
+  log = { level = "info" },
 }
 
 vim.opt.modifiable = true
@@ -36,6 +36,13 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
     vim.lsp.buf.format({ async = false })
   end,
+})
+
+-- liquid file type to html for lsp
+vim.filetype.add({
+  pattern = {
+    [".*%.html%.liquid"] = "html",
+  },
 })
 
 -- require core/ and user/
