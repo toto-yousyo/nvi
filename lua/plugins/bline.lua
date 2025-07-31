6261b5884513ca518bf745a062b51e1376bd924a
Toto <hisakazu49@gmail.com>
2025-07-31 22:14:05 +0900

    edit bufferline setting

--- lua/plugins/bline.lua
+++ lua/plugins/bline.lua
@@ -1,17 +1,21 @@
 return {
   {
-    "akinsho/bufferline.nvim", 
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
     config = function()
-    require("bufferline").setup({
-       options = {
-         mode = "tabs", 
-         separator_style = 'slant', 
-         always_show_bufferline = true, 
-         color_icons = true
-       }, 
-    })
-
-    vim.keymap.set('n', '<C-t>', '<Cmd>BufferLineCycleNext<CR>', {})
-  end 
-  } 
+      require("bufferline").setup({
+        options = {
+          mode = "buffers",
+          separator_style = { " | ", " | " },
+          diagnostics = "nvim_lsp",
+          always_show_bufferline = true,
+          color_icons = true,
+          show_tab_indicators = true,
+          show_close_icon = false,
+        },
+      })
+      vim.keymap.set("n", "<C-t>", "<Cmd>BufferLineCycleNext<CR>", {})
+    end,
+  },
 }
